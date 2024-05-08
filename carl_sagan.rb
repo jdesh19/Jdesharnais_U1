03931e02a0a27c4cf37f277f0bc7b18e195a5eed
jdesh19 <jarondesharnais@gmail.com>
2024-05-08 12:08:51 -0500

    first commit

---
+++ carl_sagan.rb
@@ -0,0 +1,21 @@
+# Using a single puts statement build the following
+# sentence using only data from the carl hash and the
+# sagan array along with some string interpolation.
+#
+# We are a way for the cosmos to know itself.
+
+carl  = {
+          :toast => 'cosmos',
+          :punctuation => [ ',', '.', '?' ],
+          :words => [ 'know', 'for', 'we']
+        }
+
+sagan = [
+          { :are => 'are', 'A' => 'a' },
+          { 'waaaaaay' => 'way', :th3 => 'the' },
+          'itself',
+          { 2 => ['to']}
+        ]
+
+puts sagan
+puts carl

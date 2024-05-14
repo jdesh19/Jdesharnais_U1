ce31807ac7426bfd2f4f465fca465e81f56dbef3
jdesh19 <jarondesharnais@gmail.com>
2024-05-14 14:17:37 -0500

    Done part 3 dog data re-formatting

--- dog_data.rb
+++ dog_data.rb
@@ -8,3 +8,11 @@ uri = URI(url)
 response = Net::HTTP.get(uri)
 dog_breeds = JSON.parse(response) # Convert JSON data into Ruby data.
 
+dog_breeds['message'].each do |dog, sub_breed|
+    if sub_breed.empty?
+        puts "* #{dog}"
+    else
+        puts "* #{dog}"
+        puts "        -#{sub_breed.join("\n\t-")}"
+    end
+end
